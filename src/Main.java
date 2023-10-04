ffbd0aaa890e57373fef361b5e9c469c4abf9a0a
Anastasiya <namirono@mail.ru>
2023-10-04 11:57:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int initialBalance = 200;
+        int addend = 1001;
+        int bonus = 0;
+        if (addend > 1000) {
+            bonus = addend/100;
+        }
+
+        int finalbalance = initialBalance + addend + bonus;
+        System.out.println("Итог: " + finalbalance);
+        System.out.println("Бонус: " + bonus);
+
+    }
+}
